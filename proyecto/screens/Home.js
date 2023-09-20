a73e638bf009f10662fbddfe21643b6d307c83a2
Gabriel Guivisdalsky <gguivisdalsky@gmail.com>
2023-09-20 10:40:24 -0300

    no funciona :(

--- proyecto/screens/Home.js
+++ proyecto/screens/Home.js
@@ -7,43 +7,32 @@ export default function Home({ navigation }) {
     const [platos, setPlatos] = useState([]);
 
     const traerTodosPlatos = async () => {
-        try {
-            const response = await getPlatos();
-            setPlatos(response.data); // Suponiendo que la respuesta de la API contiene un campo 'data' con los platos
-        } catch (error) {
-            console.error('Error al obtener los platos:', error);
-        }
+        const data = await getPlatos()
+        console.log(data)
+        setPlatos([data])
     }
+    
 
     useEffect(() => {
         traerTodosPlatos();
     }, []);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.titulo}>Challenge React Native</Text>
-            {/* Utiliza FlatList para mostrar la lista de platos */}
-            <FlatList
-                data={platos}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={({ item }) => (
-                    <View>
-                        <Text>{item.name}</Text> {/* Asegúrate de usar el campo correcto para el nombre del plato */}
-                        {/* Aquí puedes mostrar otros detalles del plato */}
-                    </View>
-                )}
-            />
-            <View style={styles.footer}>
-                <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-                    <TouchableOpacity onPress={() => { navigation.navigate("Home") }}>
-                        <Icon icon="material-symbols:home" width={"2.5rem"} />
-                    </TouchableOpacity>
-                    <TouchableOpacity onPress={() => { navigation.navigate("Buscador") }}>
-                        <Icon icon="ph:magnifying-glass-bold" width={"2.5rem"} />
-                    </TouchableOpacity>
-                </View>
-            </View>
-        </View>
+        <>
+
+        <>
+        <h5>Menú:</h5>
+        {
+            platos != null && platos.map((platos) => 
+                <div id={platos.id}>
+                    <img src={platos.image}/>
+                    <p>Nombre: {platos.title}</p>
+                    <p>Precio: ${platos.pricePerServing}</p>
+                </div>
+            )
+        }
+        </>
+        </>
     );
 }
 
