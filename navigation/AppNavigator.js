f274b3bdac93866f83d87a778dc8496b701d9669
CamilaAylenLopez <camilalopezort@gmail.com>
2023-10-09 11:31:50 -0300

    navigate arreglado

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -5,6 +5,7 @@ import { useAuth } from './AuthContext';
 import Home from '../screens/Home';
 import Login from '../screens/InicioDeSesion';
 import Info from '../screens/Info';
+import Buscador from '../screens/Buscador'
 
 const Stack = createStackNavigator();
 
@@ -19,7 +20,8 @@ const AppNavigator = () => {
                 {user ? (
                     <>
                         <Stack.Screen name="Home" component={Home} />
-                        <Stack.Screen name="Profile" component={Info} />
+                        <Stack.Screen name="Info" component={Info} />
+                        <Stack.Screen name="Buscador" component={Buscador} />
                     </>
                 ) : (
                     <>
